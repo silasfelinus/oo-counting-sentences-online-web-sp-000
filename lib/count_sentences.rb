41e2166d6f5179d2a6d9cf21f588f1b637771e3e
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-26 02:09:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ self.end_with?("!")
   end
 
   def count_sentences
-    self.split.map |self| {self.sentence? || self.question? || self.exclamation?}
+    self.split.map {self.sentence? || self.question? || self.exclamation?}
   end
 end
